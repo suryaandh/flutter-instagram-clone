cf65ed293acccd3aee5caec4233cce178edbefeb
suryaant <gdswiyasa@gmail.com>
2022-11-11 12:49:39 +0800

    change file name

---
+++ lib/utils/global_variabel.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_instagram_clone/screen/add_post_screen.dart';
+
+const webScreenSize = 600;
+
+const homeScreenItems = [
+  Text('feed'),
+  Text('a'),
+  AddPostScreen(),
+  Text('k'),
+  Text('k'),
+];
