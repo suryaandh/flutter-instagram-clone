8cd0d63f1f3bbf928ea0abbd44bd415979162140
suryaant <gdswiyasa@gmail.com>
2022-11-13 14:09:46 +0800

    create and update feed screen

--- lib/screen/feed_screen.dart
+++ lib/screen/feed_screen.dart
@@ -1,10 +1,46 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_instagram_clone/utils/colors.dart';
+import 'package:flutter_instagram_clone/widget/post_card.dart';
+import 'package:flutter_svg/svg.dart';
 
 class FeedScreen extends StatelessWidget {
   const FeedScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: mobileBackgroundColor,
+          centerTitle: false,
+          title: SvgPicture.asset(
+            'assets/ic_instagram.svg',
+            color: primaryColor,
+            height: 32,
+          ),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.messenger_outline),
+            ),
+          ],
+        ),
+        body: StreamBuilder(
+          stream: FirebaseFirestore.instance.collection('posts').snapshots(),
+          builder: (context,
+              AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+
+            return ListView.builder(
+              itemCount: snapshot.data!.docs.length,
+              itemBuilder: (context, index) =>
+                  PostCard(snap: snapshot.data!.docs[index].data()),
+            );
+          },
+        ));
   }
 }
