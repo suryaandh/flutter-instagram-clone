c181fcaef23ae7b222318d9c609fabdb939ff0d4
suryaant <gdswiyasa@gmail.com>
2022-11-22 22:32:25 +0800

    create comment section on post

--- lib/screen/comment_screen.dart
+++ lib/screen/comment_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_instagram_clone/utils/colors.dart';
 
@@ -16,6 +18,44 @@ class _CommentScreenState extends State<CommentScreen> {
         backgroundColor: mobileBackgroundColor,
         title: const Text('Comment'),
         centerTitle: false,
+      ),
+      bottomNavigationBar: SafeArea(
+        child: Container(
+          height: kToolbarHeight,
+          margin: EdgeInsets.only(
+            bottom: MediaQuery.of(context).viewInsets.bottom,
+          ),
+          padding: const EdgeInsets.only(left: 16, right: 8),
+          child: Row(
+            children: [
+              CircleAvatar(
+                backgroundImage: NetworkImage(''),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 16, right: 8),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        hintText: 'Write your comments',
+                        border: InputBorder.none),
+                  ),
+                ),
+              ),
+              InkWell(
+                onTap: () {},
+                child: Container(
+                  padding: const EdgeInsets.all(8.0),
+                  child: const Text(
+                    'Post',
+                    style: TextStyle(
+                      color: blueColor,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
